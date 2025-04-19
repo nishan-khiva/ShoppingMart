81005adb79f29d8fddf02a559e548c2c235052e2
Nishan Singh <nishankhiva@gmail.com>
2025-04-19 12:04:28 +0530

    Update Navbar.jsx

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -115,7 +115,7 @@ const Navbar = () => {
             ) : (
                 <div className='flex flex-wrap sm:flex-nowrap gap-4 sm:gap-6 items-center justify-end'>
                     <Link to='/'><h3 className='text-sm sm:text-base'>Home</h3></Link>
-                     <Link to="/seller"><h3 className='text-sm sm:text-base'>Admin Dashboard</h3></Link>
+{/*                      <Link to="/seller"><h3 className='text-sm sm:text-base'>Admin Dashboard</h3></Link> */}
                     <Link to="/all-products"><h3 className='text-sm sm:text-base'>All Product</h3></Link>
                     
 
